8df36f692a7dea1096ad6bede1e5e3594e895b98
mehdihadeli <mehdi.hadeli@outlook.com>
2020-09-11 00:24:40 +0430

    .

--- scripts/docker-publish-authserver-travis.sh
+++ scripts/docker-publish-authserver-travis.sh
@@ -16,10 +16,7 @@ esac
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 docker build -t auth-server:$DOCKER_TAG -f ./src/AuthServer/src/AuthServer/Dockerfile ./src/AuthServer
-docker build -t fibon-service:$DOCKER_TAG -f ./src/Fibon.Service/Dockerfile ./src/Fibon.Service 
 
 docker tag auth-server:$DOCKER_TAG $DOCKER_USERNAME/auth-server:$DOCKER_TAG
-docker tag fibon-service:$DOCKER_TAG $DOCKER_USERNAME/fibon-service:$DOCKER_TAG
 
 docker push $DOCKER_USERNAME/auth-server:$DOCKER_TAG
-docker push $DOCKER_USERNAME/fibon-service:$DOCKER_TAG
